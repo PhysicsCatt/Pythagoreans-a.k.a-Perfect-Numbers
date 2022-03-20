e722897ed8db622b533ae3d6a461a1fc4ab36af6
PhysicsCatt <60411441+PhysicsCatt@users.noreply.github.com>
2022-03-20 12:47:33 +0000

    Add files via upload

---
+++ main.f95
@@ -0,0 +1,18 @@
+program main
+implicit none
+
+integer :: i, j
+real :: sum
+
+do i=1,100000000
+    sum=0
+    do j=1,i-1
+        if (mod(i,j)==0) then
+            sum=sum+j
+        end if
+    end do
+    if (sum==i) then
+    print*, i, 'is a perfect number'
+    end if
+end do
+end program
